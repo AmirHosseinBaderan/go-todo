f8bdbaca65f5ce36c59749b6be6acf457d32a73b
amirhossein <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-09-13 15:28:29 +0330

    update : run server

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
 	"todo/endpoints/todo"
+
+	"github.com/gin-gonic/gin"
 )
 
-
-func main (){
+func main() {
 	r := gin.Default()
 
 	// Routes
 	todoGroup := r.Group("/todo")
 	todo.RegisterTodoEndpoints(todoGroup)
+
+	// Run application
+	r.Run(":8080")
 }
